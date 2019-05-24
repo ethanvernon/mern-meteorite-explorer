8fb79005dc506517dd8449f915eda8b957c2df46
Ethan Vernon <ethan.vernon@colorado.edu>
2019-05-24 14:22:55 +0800

    css changes, onKeyPress added to searchAPI

--- src/SearchAPI.js
+++ src/SearchAPI.js
@@ -13,6 +13,7 @@ export class SearchAPI extends Component {
 		
 		this.handleChange = this.handleChange.bind(this);
 		this.handleClick = this.handleClick.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
 
 	//passes changed value from form to Home.js
@@ -24,14 +25,20 @@ export class SearchAPI extends Component {
 	handleClick() {
 		this.props.handleClick();
 	}
+
+	handleKeyPress(target) {
+		if(target.charCode==13){
+			this.handleClick();
+		}
+	}
 	
 	render() {
 		return (
 			<div className='search-container'>
 
-				<InputGroup>
+				<InputGroup >
 
-					<Input placeholder="Type a search query here" id='home-query' type='text' value={this.props.userInput} onChange={e => this.handleChange(e)}/>
+					<Input placeholder="Type a search query here" id='home-query' type='text' value={this.props.userInput} onChange={e => this.handleChange(e)} onKeyPress={this.handleKeyPress}/>
 
 					<InputGroupAddon addonType="append">
 						<Button color='primary' onClick={this.handleClick}>search</Button>
